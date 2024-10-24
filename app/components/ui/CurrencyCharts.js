3155ff01176c75b81d0ce05a8110e466482347da
William Mercês <engenheirowilliammerces@gmail.com>
2024-10-24 15:15:30 -0500

    Dashboards on the table page

---
+++ app/components/ui/CurrencyCharts.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ClientChartsWrapper from './ClientChartsWrapper';
+
+const CurrencyCharts = () => {
+  return <ClientChartsWrapper />;
+};
+
+export default CurrencyCharts;
--- app/list/page.jsx
+++ app/list/page.jsx
@@ -1,10 +1,16 @@
-import CurrencyTable from "../components/CurrentTable";
+import CurrencyTable from '../components/CurrencyTable';
+import CurrencyCharts from '../components/ui/CurrencyCharts';
 
-export default function Page() {
+export default function Home() {
   return (
-    
-    <div className="flex flex-col h-[90vh] place-content-center">
-      <CurrencyTable />
+    <div className="min-h-screen bg-gray-50">
+      <div className="container mx-auto py-8">
+        <h1 className="text-3xl font-bold mb-8 text-center">Dashboard de Moedas</h1>
+        <CurrencyTable />
+        <div className="mt-8">
+          <CurrencyCharts />
+        </div>
+      </div>
     </div>
   );
 }
